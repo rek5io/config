26cf3e0260e8d00120c860411d3d2db4be1cef54
rek5io <janjuskowiak2004@gmail.com>
2025-08-20 10:29:35 +0200

    chore: init commit

---
+++ waybar/scripts/power-menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CHOICE=$(echo -e "exit\nreboot\nshutdown" | wofi --show dmenu --prompt "Power Menu" --style ~/.config/wofi/powermenu_style.css)
+
+case "$CHOICE" in
+exit)
+    niri msg action quit
+    ;;
+reboot)
+    systemctl reboot
+    ;;
+shutdown)
+    systemctl poweroff
+    ;;
+esac
